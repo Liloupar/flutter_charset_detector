29618e2f99a5d138d798de9f1ad687701368b1ae
zohar <hunji66@gmail.com>
2025-01-13 19:39:00 +0800

    尝试解决ios编码转换不会尽可能尝试,而是有一点错误的编码就失败的问题

--- flutter_charset_detector_darwin/darwin/Classes/SwiftFlutterCharsetDetectorPlugin.swift
+++ flutter_charset_detector_darwin/darwin/Classes/SwiftFlutterCharsetDetectorPlugin.swift
@@ -4,6 +4,8 @@
     import FlutterMacOS
 #endif
 import UniversalDetector2
+import Foundation
+import CoreFoundation
 
 public class SwiftFlutterCharsetDetectorPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -50,7 +52,19 @@ public class SwiftFlutterCharsetDetectorPlugin: NSObject, FlutterPlugin {
             return
         }
         let nsEncoding = CFStringConvertEncodingToNSStringEncoding(encoding)
-        guard let decoded = NSString(data: data.data, encoding: nsEncoding) else {
+        var decoded = NSString(data: data.data, encoding: nsEncoding)
+        if decoded == nil {
+            print("The data could not be decoded, Detected charset: \(encodingName)")
+            if encodingName == "GB18030" {
+                // 尝试修复 GB18030 编码
+                let correctedData = dataByHealingGB18030Stream(data: data.data)
+                decoded = NSString(data: correctedData, encoding: nsEncoding)
+                if decoded == nil {
+                    print("The data could not be fix, Detected charset: \(encodingName)")
+                }
+            }
+        }
+        if decoded == nil {
             result(FlutterError(code: "DecodingFailed", message: "The data could not be decoded", details: "Detected charset: \(encodingName)"))
             return
         }
@@ -78,4 +92,165 @@ public class SwiftFlutterCharsetDetectorPlugin: NSObject, FlutterPlugin {
         }
         result(encodingName);
     }
+    
+    /*
+     GB18030(兼容GB2312)编码验证与校正
+     
+     字节结构
+     单字节，其值从0到0x7F。
+     双字节，第一个字节的值从0x81到0xFE，第二个字节的值从0x40到0xFE（不包括0x7F）。
+     四字节，第一个字节的值从0x81到0xFE，第二个字节的值从0x30到0x39，第三个字节从0x81到0xFE，第四个字节从0x30到0x39。
+     因为网页 GB2312是双字节编码,所以我仅实现了GB18030单字节和双字节编码的验证与校正
+     
+     https://zh.wikipedia.org/wiki/GB_18030
+     https://zh.wikipedia.org/wiki/GB_2312
+     */
+    func replaceInvalidGB18030Charset(data: Data) -> Data {
+        var result = data
+        let replacement: [UInt8] = [65, 65, 65, 65] // 'A','A','A','A' in ASCII
+        var loc = 0
+        
+        while loc < result.count {
+            let buffer = result[loc]
+            
+            if buffer == 0xFF {
+                // 非法字符 0xFF
+                result.replaceSubrange(loc..<loc+1, with: replacement[0...0])
+                loc += 1
+            } else if (buffer & 0x80) == 0 {
+                // 单字节 ASCII 码
+                loc += 1
+            } else {
+                // 大于 0x80 的双字节或者四字节, 要根据下一位判断
+                loc += 1
+                if loc >= result.count {
+                    break
+                }
+                
+                let nextBuffer = result[loc]
+                
+                if nextBuffer != 0xFF && ((nextBuffer & 0x40) == 0x40 || (nextBuffer & 0x80) == 0x80) {
+                    // 双字节
+                    loc += 1
+                } else {
+                    // 回退第一位
+                    loc -= 1
+                    result.replaceSubrange(loc..<loc+1, with: replacement[0...0])
+                    loc += 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    
+    func dataByHealingGB18030Stream(data: Data) -> Data {
+        guard data.count > 0 else { return data }
+        
+        let replacementCharacter = "?"
+        let enc = CFStringConvertEncodingToNSStringEncoding(CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue))
+        
+        // 使用 autoreleasepool 来管理内存
+        return autoreleasepool { () -> Data in
+            let replacementCharacterData = replacementCharacter.data(using: String.Encoding(rawValue: enc))!
+            
+            let resultData = NSMutableData(capacity: data.count)!
+            let bytes = [UInt8](data)
+            
+            let bufferMaxSize = 1024
+            var buffer = [UInt8](repeating: 0, count: bufferMaxSize)
+            var bufferIndex = 0
+            var byteIndex = 0
+            var invalidByte = false
+            
+            func flushBuffer() {
+                if bufferIndex > 0 {
+                    resultData.append(&buffer, length: bufferIndex)
+                    bufferIndex = 0
+                }
+            }
+            
+            func checkBuffer() {
+                if (bufferIndex + 5) >= bufferMaxSize {
+                    resultData.append(&buffer, length: bufferIndex)
+                    bufferIndex = 0
+                }
+            }
+            
+            while byteIndex < data.count {
+                let byte = bytes[byteIndex]
+                
+                if byte >= 0 && byte <= 0x7f {
+                    checkBuffer()
+                    buffer[bufferIndex] = byte
+                    bufferIndex += 1
+                } else if byte >= 0x81 && byte <= 0xfe {
+                    if byteIndex + 1 >= data.count {
+                        flushBuffer()
+                        return resultData as Data
+                    }
+                    
+                    let byte2 = bytes[byteIndex + 1]
+                    if byte2 >= 0x40 && byte2 <= 0xfe && byte2 != 0x7f {
+                        let tuple: [UInt8] = [byte, byte2]
+                        if let cfstr = CFStringCreateWithBytes(kCFAllocatorDefault, tuple, 2, CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue), false) {
+                            //                    CFRelease(cfstr)
+                            checkBuffer()
+                            buffer[bufferIndex] = byte
+                            buffer[bufferIndex + 1] = byte2
+                            bufferIndex += 2
+                            byteIndex += 1
+                        } else {
+                            invalidByte = true
+                        }
+                    } else if byte2 >= 0x30 && byte2 <= 0x39 {
+                        if byteIndex + 3 >= data.count {
+                            flushBuffer()
+                            return resultData as Data
+                        }
+                        
+                        let byte3 = bytes[byteIndex + 2]
+                        
+                        if byte3 >= 0x81 && byte3 <= 0xfe {
+                            let byte4 = bytes[byteIndex + 3]
+                            
+                            if byte4 >= 0x30 && byte4 <= 0x39 {
+                                let tuple: [UInt8] = [byte, byte2, byte3, byte4]
+                                if let cfstr = CFStringCreateWithBytes(kCFAllocatorDefault, tuple, 4, CFStringEncoding(CFStringEncodings.GB_18030_2000.rawValue), false) {
+                                    //                            CFRelease(cfstr)
+                                    checkBuffer()
+                                    buffer[bufferIndex] = byte
+                                    buffer[bufferIndex + 1] = byte2
+                                    buffer[bufferIndex + 2] = byte3
+                                    buffer[bufferIndex + 3] = byte4
+                                    bufferIndex += 4
+                                    byteIndex += 3
+                                } else {
+                                    invalidByte = true
+                                }
+                            } else {
+                                invalidByte = true
+                            }
+                        } else {
+                            invalidByte = true
+                        }
+                    } else {
+                        invalidByte = true
+                    }
+                    
+                    if invalidByte {
+                        invalidByte = false
+                        flushBuffer()
+                        resultData.append(replacementCharacterData)
+                    }
+                }
+                byteIndex += 1
+            }
+            
+            flushBuffer()
+            return resultData as Data
+        }
+    }
+    
 }
